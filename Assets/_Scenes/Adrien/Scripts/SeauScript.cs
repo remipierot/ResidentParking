49481b7288d77d18415b0e8c829d7f10ea9ba41d
Adrien <meuseuneu@hotmail.fr>
2017-02-17 14:47:56 +0100

    Son seau plus anim.

---
+++ Assets/_Scenes/Adrien/Scripts/SeauScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SeauScript : MonoBehaviour {
+
+    public AudioSource mAudioSource;
+    public AudioClip[] soundsSeau;
+
+	// Use this for initialization
+	void Start () {
+        mAudioSource = GetComponent<AudioSource>();
+
+    }
+	
+	// Update is called once per frame
+	void OnCollisionEnter (Collision col) {
+        Debug.Log("COUCOUZZZ");
+
+        int r = Random.Range(0, 1);
+        mAudioSource.clip = soundsSeau[r];
+
+        if (col.gameObject.tag.Equals("Player") && !mAudioSource.isPlaying)
+        {
+            mAudioSource.Play();
+        }
+    }
+}
