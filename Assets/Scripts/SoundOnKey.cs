6c4dd57ba3ae4bda19677b1bacf087f41d9a999e
Axel Deret <deretaxel@gmail.com>
2017-02-17 14:34:26 +0100

    Son ambiant vent + pluie + son pulse

---
+++ Assets/Scripts/SoundOnKey.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(AudioSource))]
+public class SoundOnKey : MonoBehaviour
+{
+
+    [SerializeField]
+    private AudioClip m_AmbiantSounds;
+    [SerializeField]
+    private AudioSource m_AudioSource;
+
+
+    void Start()
+    {
+        m_AudioSource.clip = m_AmbiantSounds;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            m_AudioSource.Play();
+        }
+    }
+}
