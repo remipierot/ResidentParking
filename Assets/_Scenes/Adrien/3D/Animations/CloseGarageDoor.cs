c0d14c6de4c1850487955d9948936d2ac7038839
Adrien <meuseuneu@hotmail.fr>
2017-02-17 13:46:26 +0100

    Ajout seau, portes, anim.

---
+++ Assets/_Scenes/Adrien/3D/Animations/CloseGarageDoor.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloseGarageDoor : MonoBehaviour {
+
+    public Animator GarageDoorAnimator;
+    public AnimationClip GarageDoorClosingCiip;
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag.Equals("Player"))
+        {
+            GarageDoorAnimator.SetBool("Closing", true);
+        }
+    }
+
+}
